57e8dc8c690a589c477f63e19852dc9fae08468b
Isaacherna224 <118489250+Isaacherna224@users.noreply.github.com>
2024-01-24 06:23:28 -0600

    solo falta la foto y hay por ahi algo que evaluar con el correo, su validacion se hace despúes del return del happy-path

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -24,67 +24,118 @@
           
         ></b-form-input>
       </b-form-group>
-
-      <b-form-group id="input-group-2" label="Your surname:" label-for="input-2">
+      <b-form-group id="surname" label="Your surname:" label-for="surname">
         <b-form-input
-          id="input-2"
-          v-model="form.surname"
-          placeholder="Enter surname"
+            id="surname"
+            v-model="form.surname"
+            placeholder="Enter surname"
         ></b-form-input>
       </b-form-group>
 
+      <b-form-row>
+        <b-form-group
+            id="cp"
+            label="Your CP:"
+            label-for="cp"
+        >
+          <b-form-input
+              id="cp"
+              v-model="form.direccion.cp"
+              placeholder="Enter cp"
+
+          ></b-form-input>
+          <b-form-group
+              id="calle"
+              label="Your Street:"
+              label-for="calle"
+          >
+            <b-form-input
+                id="calle"
+                v-model="form.direccion.calle"
+                placeholder="Enter street"
+            ></b-form-input>
+          </b-form-group>
+
+          <!-- Componente para el número exterior -->
+          <b-form-group
+              id="num_ext"
+              label="Your Exterior Number:"
+              label-for="num_ext"
+          >
+            <b-form-input
+                id="num_ext"
+                v-model="form.direccion.num_ext"
+                placeholder="Enter exterior number"
+            ></b-form-input>
+          </b-form-group>
+
+          <!-- Componente para la ciudad -->
+          <b-form-group
+              id="ciudad"
+              label="Your City:"
+              label-for="ciudad"
+          >
+            <b-form-input
+                id="ciudad"
+                v-model="form.direccion.ciudad"
+                placeholder="Enter city"
+            ></b-form-input>
+          </b-form-group>
+        </b-form-group>
+
+      </b-form-row>
 
       <b-form-group
-        id="input-group-2"
-        label="Your direccion:"
-        label-for="input-2"
+          id="fec_nac"
+          label="Your fecha de nacimiento:"
+          label-for="fec_nac"
       >
         <b-form-input
-          id="input-2"
-          v-model="form.direccion"
-          placeholder="Enter direccion"
-          
+            id="fec_nac"
+            v-model="form.fec_nac"
+            placeholder="Enter Fecha de nacimineto"
+
+            type="date"
         ></b-form-input>
       </b-form-group>
 
       <b-form-group
-        id="input-group-2"
-        label="Your fecha de nacimiento:"
-        label-for="input-2"
+          id="tel"
+          label="Your telefono:"
+          label-for="tel"
       >
         <b-form-input
-          id="input-2"
-          v-model="form.fec_nac"
-          placeholder="Enter Fecha de nacimineto"
-          
-          type="date"
+            id="tel"
+            v-model="form.tel"
+            placeholder="Enter Telefono"
         ></b-form-input>
       </b-form-group>
 
       <b-form-group
-        id="input-group-2"
-        label="Your telefono:"
-        label-for="input-2"
+          id="email"
+          label="Email address:"
+          label-for="email"
+          description="We'll never share your email with anyone else."
       >
         <b-form-input
-          id="input-2"
-          v-model="form.tel"
-          placeholder="Enter Telefono"
+            id="email"
+            v-model="form.email"
+            type="email"
+            placeholder="Enter email"
+
         ></b-form-input>
       </b-form-group>
-
       <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
+          id="foto"
+          label="Your foto:"
+          label-for="foto"
       >
         <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          placeholder="Enter email"
-          
+            id="foto"
+            v-model="form.foto"
+            placeholder="Enter foto"
+            type="file"
+            accept="image/*"
         ></b-form-input>
       </b-form-group>
 
@@ -107,7 +158,12 @@ export default {
         name: "",
         lastname: "",
         surname: "",
-        direccion: "",
+        direccion:{
+          cp: "",
+          calle:"",
+          num_ext: "",
+          ciudad: ""
+        },
         fec_nac: "",
         tel: "",
         foto: "",
@@ -116,45 +172,79 @@ export default {
   },
   methods: {
     checkForm(e) {
+      this.errors = [];
       if (
-        this.form.name,
-        this.lastname &&
-        this.email &&
-        this.fec_nac &&
-        this.direccion &&
-        this.tel
+          this.form.name &&
+          this.form.lastname &&
+          this.form.email &&
+          this.form.fec_nac &&
+          this.form.direccion.cp &&
+          this.form.direccion.calle &&
+          this.form.direccion.num_ext &&
+          this.form.direccion.ciudad &&
+          this.form.tel
       ) {
+        console.log('logra');
 
         return true;
       }
 
-      this.errors = [];
 
-      if (!this.name) {
-        console.log('nombre');
+
+      if (!this.form.name) {
         this.errors.push("El nombre es obligatorio.");
-      }
-      if (!this.lastname) {
-        this.errors.push("El apellido paterno es obligatoria.");
-      }
+      } else if (this.checkIsString(this.form.name)) {
+        this.errors.push("El nombre debe ser válido.");}
 
-      if (!this.email) {
-        this.errors.push('El correo electrónico es obligatorio.');
-      } else if (!this.validEmail(this.email)) {
-        this.errors.push('El correo electrónico debe ser válido.');
-      }
+       if (!this.form.lastname) {
+         this.errors.push("El apellido paterno es obligatoria.");
+       } else if (this.checkIsString(this.form.lastname)) {
+         this.errors.push("El apellido paterno debe ser válido.");
+       }
 
-      if (!this.fec_nac) {
-        this.errors.push("La fecha de nacimiento es obligatoria.");
-      }
+       if(!this.form.direccion.cp){
+         this.errors.push("El código postal es obligatorio.");
+       } else if(this.form.direccion.cp.length != 5){
+         this.errors.push("El código postal debe tener 5 dígitos.");
+       }
 
-      if (!this.direccion) {
-        this.errors.push("La dirección es obligatoria.");
-      }
+        if(!this.form.direccion.calle){
+          this.errors.push("La calle es obligatoria.");
+        }
 
-      if (!this.tel) {
-        this.errors.push("El número de teléfono es obligatorio.");
-      }
+        if(!this.form.direccion.num_ext){
+          this.errors.push("El número exterior es obligatorio.");
+        }
+
+        if(!this.form.direccion.ciudad){
+          this.errors.push("La ciudad es obligatoria.");
+        }
+
+
+        if(!this.form.fec_nac){
+          this.errors.push("La fecha de nacimiento es obligatoria.");
+        }
+
+        if(this.form.fec_nac){
+          var fecha_actual = new Date();
+          var fecha_nac = new Date(this.form.fec_nac);
+          var edad = fecha_actual.getFullYear() - fecha_nac.getFullYear();
+          if(edad > 18){
+            this.errors.push("Lo sentimos, limite de 18 años.");
+          }
+        }
+
+        if (!this.form.tel) {
+          this.errors.push("El número de teléfono es obligatorio.");
+        } else if (this.form.tel.length != 10) {
+          this.errors.push("El número de teléfono debe tener 10 dígitos.");
+        }
+
+        if (!this.form.email) {
+          this.errors.push("El correo electrónico es obligatorio.");
+        } else if (!this.checkEmail(this.form.email)) {
+          this.errors.push("El correo electrónico debe ser válido.");
+        }
 
       //   if (!this.foto) {
       //     this.errors.push("La imagen es obligatoria.");
@@ -166,6 +256,9 @@ export default {
       var re =
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
+    },
+    checkIsString(value) {
+      return typeof value === "string" || value instanceof String;
     },
   },
 };
